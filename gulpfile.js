7888457a867a09d93ece2c2eaba754a7029a18fd
Denis Jankelaic <d3nziss@gmail.com>
2018-06-28 18:09:49 +0300

    init commit

--- gulpfile.js
+++
@@ -1,1 +0,0 @@
-require("simplr-gulp");
--- src/app/components/Posts/src/Post.ts
+++
@@ -1,6 +0,0 @@
-export interface Post {
-    Post_title?: string | null;
-    Post_description?: string | null;
-    Post_descriptiontwo?: string | null;
-    url: string;
-}
